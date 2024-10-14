1f6f4ab1fcb26f8252bd68cb9be0db8e63a5a87d
Znorlux <christianlombardi0414@gmail.com>
2024-10-13 22:49:17 -0500

    feat: Actualizar icono de créditos en la página de perfil
    Se actualizó el icono de los créditos en la página de perfil para utilizar una imagen más representativa.

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,6 +1,6 @@
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
-//import CreditIcon from "../../../public/credit-icon.png"; // Icono para los créditos
+import CreditIcon from "../../../public/qc-coin.png"; // Icono para los créditos
 import StatusIcon from "../../../public/status-icon.png"; // Icono para el estatus
 import AchievementIcon from "../../../public/holo-red.png"; // Icono para los logros
 import { Separator } from "@/components/ui/separator";
@@ -48,7 +48,13 @@ export default function Profile({ params }: { params: { username: string } }) {
         {/* Créditos Cuánticos */}
         <Card className="p-4">
           <h2 className="text-lg font-bold mb-2 flex items-center gap-2">
-            <QCIcon /> Créditos Cuánticos
+            <Image
+              src={CreditIcon}
+              alt="Quantum Credits"
+              width={30}
+              height={30}
+            />{" "}
+            Créditos Cuánticos
           </h2>
           <p className="text-blue-600 text-2xl font-bold">
             {userData.quantumCredits} QCs
@@ -144,33 +150,3 @@ export default function Profile({ params }: { params: { username: string } }) {
     </div>
   );
 }
-
-const QCIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={30}
-    height={30}
-    color={"#000000"}
-    fill={"none"}
-  >
-    <path
-      d="M14 18C18.4183 18 22 14.4183 22 10C22 5.58172 18.4183 2 14 2C9.58172 2 6 5.58172 6 10C6 14.4183 9.58172 18 14 18Z"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-    />
-    <path
-      d="M3.1004 11C2.4033 12.1065 2 13.4168 2 14.8212C2 18.7859 5.21417 22 9.17905 22C10.5834 22 11.8935 21.5968 13 20.8998"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-    />
-    <path
-      d="M12 10H15.5M12 10V6.5H14M12 10V13.5H14M15.5 10C16.3284 10 17 9.2165 17 8.25C17 7.2835 16.3284 6.5 15.5 6.5H14M15.5 10C16.3284 10 17 10.7835 17 11.75C17 12.7165 16.3284 13.5 15.5 13.5H14M14 13.5V14.5M14 6.5V5.5"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-    />
-  </svg>
-);

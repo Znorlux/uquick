6ddd7f4cda04bf49c20e396b9ecf7cec29b4f246
Znorlux <christianlombardi0414@gmail.com>
2024-11-03 21:55:54 -0500

    Agregar validaciones para CLIENT_ID y ADMIN_PRIVATE_KEY en client.ts

--- app/client.ts
+++ app/client.ts
@@ -3,11 +3,18 @@ import { avalancheFuji } from "thirdweb/chains";
 import { privateKeyToAccount } from "thirdweb/wallets";
 
 const clientID = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
-const ADMIN_PRIVATE_KEY = process.env.NEXT_PUBLIC_ADMIN_KEY;
+const ADMIN_PRIVATE_KEY = process.env.ADMIN_PRIVATE_KEY;
+
+if (!clientID) {
+  throw new Error("Missing CLIENT_ID");
+}
+if (!ADMIN_PRIVATE_KEY) {
+  throw new Error("Missing ADMIN_PRIVATE_KEY");
+}
 
 // create the client with your clientId, or secretKey if in a server environment
 export const client = createThirdwebClient({
-  clientId: clientID!,
+  clientId: clientID,
 });
 
 export const contract = getContract({
@@ -18,5 +25,5 @@ export const contract = getContract({
 
 export const account = privateKeyToAccount({
   client,
-  privateKey: ADMIN_PRIVATE_KEY!,
+  privateKey: ADMIN_PRIVATE_KEY,
 });
